b11dc6bf4952e1a9f13046cc7704b9ec5e35a9fc
rivedroz23 <river.droz@gmail.com>
2019-08-13 16:26:02 -0700

    first commit

---
+++ src/routes/api.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+const router = express.Router();
+import User from '../models/user';
+import axios from 'axios';
+
+router.get('/:id/repos', (req, res) => {
+    let config = {
+        headers: {
+            'Authorization': `Bearer ${req.user.accessToken}`,
+            'User-Agent': 'river-githubproject'
+        }
+    }
+    axios.get(`https://api.github.com/user/repos`, config)
+        .then((response) => {
+            res.json(response.data);
+        }).catch((err) => {
+            console.log(err)
+        })
+});
+
+
+export default router;
